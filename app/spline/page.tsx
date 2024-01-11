0443dfa1d0ac4022fcc84ec4b1d7155c9dba0533
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2024-01-12 01:01:01 +0200

    Testing Spline

---
+++ app/spline/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import Spline from '@splinetool/react-spline';
+
+export default function Page() {
+  return (
+    <Spline scene="https://prod.spline.design/8Klt5K6yz4OLjgmM/scene.splinecode" />
+  );
+}
