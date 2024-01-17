f7a403481ebc3e1320951596865d792863d4910a
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2024-01-17 23:14:49 +0200

    Created Invoice forms

--- app/spline/page.tsx
+++ app/spline/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useRef } from 'react';
 import Spline from '@splinetool/react-spline';
 
 export default function Page() {
@@ -7,3 +8,16 @@ export default function Page() {
     <Spline scene="https://prod.spline.design/8Klt5K6yz4OLjgmM/scene.splinecode" />
   );
 }
+
+// import Image from 'next/image';
+
+// const test = () => {
+//   return (
+//     <div className="h-96 w-96 border-red-50">
+//       Room Page
+//       <Image src="/room.Webp" alt="room" width={500} height={500} />
+//     </div>
+//   );
+// };
+
+// export default test;
